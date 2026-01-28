84e68ac8a76c36f6f12966d6cba60808dc461268
Aaron Martirez <aaronmartirez.26@gmail.com>
2026-01-28 19:07:02 +0800

    feat(frontend): create custom axios

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+
+const API = axios.create({
+    baseURL: process.env.VITE_API_URL,
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    timeout: 10000, // 10 seconds timeout
+});
+
+/*
+    - If access token expired (404)
+    - Call /auth/refresh-token
+    - Retry original request
+ */
+API.interceptors.response.use(
+
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (
+            error.response?.status === 401 &&
+            !originalRequest._retry
+        ) {
+            originalRequest._retry = true;
+
+            try {
+                await API.post('/auth/refresh-token');
+                return API(originalRequest);
+            } catch (error) {
+                window.location.href = '/login';
+            }
+        }
+
+        return Promise.reject(error);
+    }
+
+);
+
+export default API;
