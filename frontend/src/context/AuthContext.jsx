eeea676e7a8967c829bdd0ff22a4bba5574b2314
Aaron Martirez <aaronmartirez.26@gmail.com>
2026-01-28 19:48:04 +0800

    feat(frontend): create AuthContext with login and logout action

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,51 @@
+import { createContext, useEffect, useState } from "react";
+import API from "../api/API";
+
+
+const AuthContext = () => {
+    
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    /*
+        - Called on app load
+        - Checks if cookies are valid
+        - If valid, fetch user data and set user state
+        - If not valid, set user to null
+     */
+    useEffect(() => {
+        const loadUser = async () => {
+            try {
+                const response = await API.get("/auth/me");
+                setUser(response.data.user);
+            } catch (error) { 
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadUser();
+    }, []);
+
+    // Login function
+    const login = async (data) => {
+        const response = await API.post("/auth/login", data);
+        setUser(response.data.user);
+    };
+
+    // Logout function
+    const logout = async () => {
+        await API.post("/auth/logout");
+        setUser(null);
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, loading, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+};
+
+export default AuthContext;
