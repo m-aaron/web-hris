cd497e9fa97f7fc9a14aad8f8174d30d6d8a299f
Aaron Martirez <aaronmartirez.26@gmail.com>
2026-02-23 16:21:42 +0800

    feat(backend): add update employee personal data endpoint

--- backend/src/routes/personalRoute.js
+++ backend/src/routes/personalRoute.js
@@ -2,11 +2,12 @@ import express from 'express';
 import { ROLES } from "../constants/roleConstant.js";
 import { authenticate } from "../middlewares/authenticateMiddleware.js";
 import { authorizeRoles } from "../middlewares/authorizeMiddleware.js";
-import { savePersonalInfo } from '../controllers/personalController.js';
+import { savePersonalInfo, updatePersonalInfo } from '../controllers/personalController.js';
 
 
 const router = express.Router();
 
 router.put('/:id/personal', authenticate, authorizeRoles(ROLES.ADMIN, ROLES.HR), savePersonalInfo);
+router.put('/:id/personal/update', authenticate, authorizeRoles(ROLES.ADMIN, ROLES.HR), updatePersonalInfo);
 
 export default router;
