bf8b6aaf5c9ea4fec9f5965ae20a92789e86ec06
Aaron Martirez <aaronmartirez.26@gmail.com>
2026-02-23 16:42:24 +0800

    feat(backend): add update employee family background endpoint

--- backend/src/routes/familyRoute.js
+++ backend/src/routes/familyRoute.js
@@ -2,11 +2,12 @@ import express from 'express';
 import { ROLES } from "../constants/roleConstant.js";
 import { authenticate } from "../middlewares/authenticateMiddleware.js";
 import { authorizeRoles } from "../middlewares/authorizeMiddleware.js";
-import { saveFamilyBackground } from '../controllers/familyController.js';
+import { saveFamilyBackground, updateFamilyBackground } from '../controllers/familyController.js';
 
 
 const router = express.Router();
 
 router.put('/:id/family', authenticate, authorizeRoles(ROLES.ADMIN, ROLES.HR), saveFamilyBackground);
+router.put('/:id/family/update', authenticate, authorizeRoles(ROLES.ADMIN, ROLES.HR), updateFamilyBackground);
 
 export default router;
